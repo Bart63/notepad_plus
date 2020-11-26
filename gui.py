c0fb7f5d9f3f59547857e9db7be6d638938b7430
Kerinjo <11coonk@gmail.com>
2020-11-26 14:42:37 +0100

    prototype main window gui with note class

--- gui.py
+++ gui.py
@@ -1,6 +1,4 @@
 from tkinter import *
-from tkinter import filedialog
-import pickle
 
 noteList = []
 
@@ -13,50 +11,36 @@ def addNote():
 	note = Note()
 	noteList.append(note)
 
+def switchPage():
 
-class Note:
+
+class tkNote:
 	
 	def __init__ (self):
 		frame = Frame(root)
 		frame.pack()
 
 		self.noteButton = Label(frame, text = "Title")
-		self.noteButton.pack(side = LEFT)
+		self.noteButton.pack(side= LEFT)
 
-		self.delButton  = Button(frame, text = "delete", command = self.delMe)
+
+		self.delButton  = Button(frame, text = "delete", command=self.delMe)
 		self.delButton.pack(side = LEFT)
 
-		self.savebutton = Button(text = "save", command = self.save)
-		self.savebutton.pack(side = LEFT)
-
-		self.window = Tk()
-		self.text = Text(self.window)
-		self.text.pack()
 
 	def __del__(self):
-		print("Destructor called.")
+		print("Bye.")
 
 	def delMe(self):
 		self.noteButton.pack_forget()
 		self.delButton.pack_forget()
-		print("Bye.")
 		del self
-
-	def save(self):
-		file = filedialog.asksaveasfile(defaultextension = ".txt",
-										filetypes = [
-											("text file",".txt"),
-											("read only file", ".odt"),
-											("PDF file", ".pdf"),
-										])
-		filetext = str(self.text.get(1.0,END))
-		self.file.write(filetext)
-		self.file.close()
-
 
 
 addButton = Button(topFrame, text='Add', command = addNote)
 addButton.pack()
 
+switchButton = Button(text = 'Switch', command = switchPage)
+switchButton.pack()
 
 root.mainloop()
